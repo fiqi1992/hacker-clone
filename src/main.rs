c85346f521f4854a5c9fc86f2f8cd1038408de00
fiqi1992 <fahrir20@gmail.com>
2020-11-16 17:38:30 +0700

    membuat web menggunakan rust

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use actix_web::{HttpServer, App, web, HttpResponse, Responder};
+use tera::{Tera, Context};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize)]
+struct Post {
+    title: String,
+    link: String,
+    author: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct User {
+    username: String,
+    email: String,
+    password: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct LoginUser {
+    username: String,
+    password: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Submission {
+    title: String,
+    link: String,
+}
+
+async fn submission(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Submit a Post");
+
+    let rendered = tera.render("submission.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn process_submission(data: web::Form<Submission>) -> impl Responder {
+    println!("{:?}", data);
+    HttpResponse::Ok().body(format!("Posted submission: {}", data.title))
+}
+
+async fn login(tera: web::Data<Tera>) ->impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Login");
+
+    let rendered = tera.render("login.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn process_login(data: web::Form<LoginUser>) -> impl Responder {
+    println!("{:?}", data);
+    HttpResponse::Ok().body(format!("Logged in: {}", data.username))
+}
+
+async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+
+    let posts = [
+        Post {
+            title: String::from("this is the first link"),
+            link: String::from("https://example.com"),
+            author: String::from("Fahri")
+        },
+        Post {
+            title: String::from("The second link"),
+            link: String::from("https://example.com"),
+            author: String::from("Icha")
+        },
+    ];
+
+    data.insert("title", "Hacker Clone");
+    data.insert("posts", &posts);
+
+    let rendered = tera.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn signup(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Sign Up");
+
+    let rendered = tera.render("signup.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+async fn process_signup(data: web::Form<User>) -> impl Responder {
+    println!("{:?}", data);
+    HttpResponse::Ok().body(format!("Successfully saved user: {}", data.username))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        //memberikan ** agar tera bisa mengakses subfolder
+        let tera = Tera::new("templates/**/*").unwrap();
+        App::new()
+        .data(tera)
+        .route("/", web::get().to(index))
+        .route("/signup", web::get().to(signup)) //menambah route signup
+        .route("/signup", web::post().to(process_signup)) //menambah route process signup
+        .route("/login", web::get().to(login))
+        .route("/login", web::post().to(process_login))
+        .route("/submission", web::get().to(submission))
+        .route("/submission", web::post().to(process_submission))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
